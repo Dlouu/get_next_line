78216391778c53ac7c3947da5f70e6c5de1c8478
Dlouu <135530996+Dlouu@users.noreply.github.com>
2024-01-16 15:25:21 +0100

    kek_next_line OK 125/125

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbaumgar <mbaumgar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/02 17:40:41 by mbaumgar          #+#    #+#             */
+/*   Updated: 2024/01/15 17:07:10 by mbaumgar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+// get_next_line
+char	*get_next_line(int fd);
+
+// libft utils
+void	*ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbaumgar <mbaumgar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/02 17:40:26 by mbaumgar          #+#    #+#             */
+/*   Updated: 2024/01/15 17:06:33 by mbaumgar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (i < n)
+	{
+		((unsigned char *)s)[i] = '\0';
+		i++;
+	}
+	return (s);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if ((unsigned char)c == '\0' && *s == '\0')
+		return ((char *)s);
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+			return ((char *)s);
+		if (*(s + 1) == (unsigned char)c)
+			return ((char *)s + 1);
+		s++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		size_s1;
+	int		size;
+	int		i;
+
+	i = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (s2);
+	if (!s2)
+		return (s1);
+	size_s1 = ft_strlen(s1);
+	size = size_s1 + ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	while (i < size_s1)
+		str[i++] = *s1++;
+	while (i < size)
+		str[i++] = *s2++;
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	s_len;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (len > s_len - start)
+		len = s_len - start;
+	if ((len == 1 && s[0] == '\0') || start >= s_len)
+		len = 0;
+	else
+		s += start;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (s && i < len)
+		str[i++] = *s++;
+	str[i] = '\0';
+	return (str);
+}
